a1bf1d9c0ade6122b2454ae82dee7ec916bc031d
Michael Dasar <michaelgdasaro@gmail.com>
2021-10-16 13:49:00 -0400

    decent ground generation (bad fps from speed)

--- Haxle.py
+++ Haxle.py
@@ -4,28 +4,66 @@ import math
 import random
 import numpy
 
+#Initialization and global vars
 pygame.init()
 clock = pygame.time.Clock()
 pygame.display.set_caption('Haxle')
 windowX = 1600
 windowY = 900
-
+startYOffset = .9
+ticks=0
 window_surface = pygame.display.set_mode((windowX, windowY))
-camera_speed = 30
-background_color = 'SeaGreen'#'DarkOliveGreen'#'MediumSeaGreen'
+camera_speed = 15
+background_color = 'SeaGreen'
 background = pygame.Surface((windowX, windowY))
 background.fill(pygame.Color(background_color))
+truck = pygame.image.load('truck.png')
+truck.convert_alpha()
+truckX = 100
+truckY = windowY*startYOffset - truck.get_size()[1]
 
 manager = pygame_gui.UIManager((windowX, windowY))
 
+points = [(0,windowY*startYOffset)]
+lastY = windowY*startYOffset
+bumpiness = .1
+smoothness = 40
+for i in range(1,windowX,smoothness):
+    dy = random.uniform(-1*bumpiness,bumpiness)
+
+    points.append((i,int(lastY+dy)))
+    lastY = lastY+dy
+
+
+class Truck(pygame.sprite.Sprite):
+    def __init__(self, color, width, height):
+        pygame.sprite.Sprite.__init__(self)
+
+
 
 is_running = True
-
+timing = 0
 while is_running:
-    
+    background.blit(truck,(truckX,truckY))
+    ticks+=1
+    if bumpiness < 15: bumpiness+=(ticks/100000)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
+    ms = clock.tick(60)
     window_surface.blit(background, (0, 0))
+    
     background.fill(pygame.Color(background_color))
+    pygame.draw.lines(background, (200,200,200), False, points, width=1)
+    timing += camera_speed
+    if timing >= 60:
+        timing -=60
+        for i in range(0,len(points)-1):
+            points[i] = (points[i][0],points[i+1][1])
+    newY = int(random.uniform(-1*bumpiness,bumpiness+1)+points[len(points)-1][1])
+    if newY >= windowY-1: newY=windowY-1
+    elif newY <= 0: newY=0
+    points[len(points)-1]=(windowX,newY)
+
+    
     pygame.display.update()
